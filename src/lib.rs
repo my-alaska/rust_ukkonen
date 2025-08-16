52c41ac453e439c1a477668443d058e7d1a1d595
my-alaska <piotrztych@wp.pl>
2025-08-16 21:33:21 +0200

    add debug comments

--- src/lib.rs
+++ src/lib.rs
@@ -49,11 +49,14 @@ impl<'a, T: Eq + Hash + Display> UkkonenTree<'a, T> {
     }
 
     fn walk_down(&mut self) -> bool {
+        println!("    walking down");
         let element = self.sequence[self.active_edge.unwrap()];
 
         let children = self.active_node.children.borrow();
 
         if !children.contains_key(element) {
+
+            println!("    walk down failed - no child in keys");
             return false;
         }
 
@@ -62,18 +65,21 @@ impl<'a, T: Eq + Hash + Display> UkkonenTree<'a, T> {
         // Otherwise compiler won't let us assign the active node
         drop(children);
 
-        let edge_length = active_child.end_idx.unwrap() - *active_child.start_idx.borrow();
+        if let Some(end_idx) = active_child.end_idx{
+            let edge_length = end_idx - *active_child.start_idx.borrow();
+            if self.active_length >= edge_length {
 
-        if self.active_length >= edge_length {
-            if let Some(_) = active_child.end_idx{
                 self.active_edge = Some(self.active_edge.unwrap() + edge_length);
                 self.active_length -= edge_length;
                 self.active_node = active_child;
 
+                println!("    walk down successful");
                 return true
             }
+            println!("    walk down failed - leaf node");
         }
 
+        println!("    walk down failed");
         false
     }
 
@@ -98,6 +104,9 @@ impl<'a, T: Eq + Hash + Display> UkkonenTree<'a, T> {
 
     // This method can be used to extend existing tree with new elements
     pub fn extend_tree(&mut self, element: &'a T) {
+        println!();
+        println!("creating node for element {}", element);
+
         self.sequence.push(element);
 
         // initialize the checker for the most recently created node
@@ -107,47 +116,66 @@ impl<'a, T: Eq + Hash + Display> UkkonenTree<'a, T> {
         // It's the number of characters to process or implicitly stored
         self.remainder += 1;
         while self.remainder > 0 {
+            println!("starting loop with: ");
+            if let Some(active_edge) = self.active_edge {
+                println!("    active edge: {} ", self.sequence[active_edge]);
+            } else {
+                println!("    active edge: None ");
+            }
+            println!("    remainder: {}", self.remainder);
+            println!("    active length: {}",self.active_length);
 
-            // If the active node doesn't have edge corresponding to our letter
-            if self.active_length == 0 {
 
+            if self.active_length == 0{
                 // If active length is 0 we select active edge
                 self.active_edge = Some(self.sequence.len() - 1);
 
-                // If active node has no edge corresponding to `element`...
-                if !self.active_node.children.borrow().contains_key(element)  {
-                    // create a new child node and edge that does
 
-                    // initialize node
-                    let new_node = Node::new(self.sequence.len() - 1);
-                    let new_node = Rc::new(new_node);
+                println!("    active length is zero!");
+                println!("    active edge updated to {}", self.sequence[self.active_edge.unwrap()]);
+            }
 
-                    // and input it as a child into the active node.
-                    // We need to borrow a mutable reference to the children hashmap
-                    self.active_node.children.borrow_mut().insert(element, new_node);
-                }
+            // If the active node doesn't have edge corresponding to our letter
+            if self.active_length == 0 && !self.active_node.children.borrow().contains_key(element){
+                println!("    element NOT found in active node keys");
+                // create a new child node and edge that does
+
+                // initialize node
+                let new_node = Node::new(self.sequence.len() - 1);
+                let new_node = Rc::new(new_node);
+
+                // and input it as a child into the active node.
+                // We need to borrow a mutable reference to the children hashmap
+                self.active_node.children.borrow_mut().insert(element, new_node);
+
 
                 // If new node was created in previous iterations
                 // create a special link from it to current active node
                 if !Rc::ptr_eq(&self.root, &self.active_node){
+                    println!("    creating a special link");
+
                     self.link_created_node(last_created_node, self.active_node.clone());
                     last_created_node = None;
                 }
 
             // If there is active length
             } else {
+                println!("    element found in active node keys");
+
                 // check if we can "walk down" - active length is greater than the length of edge below
                 // If we walk down we have to process the beginning of the loop
                 if self.walk_down() { continue }
 
                 // extract the child at the end of the active edge. It has some important information
-                let children_ref = self.active_node.children.borrow_mut();
+                let mut children_ref = self.active_node.children.borrow_mut();
                 let active_child = children_ref.get(element).unwrap();
 
                 // In case our new letter is already implicitly represented on the active edge
                 // We want to update active length (and create a link if need be)
                 // We break out of the loop here. It's the default case of implicit extension
                 if self.sequence[*active_child.start_idx.borrow() + self.active_length] == element {
+                    println!("    active sequence extended implicitly");
+
                     // Update link of last_created_node
                     self.link_created_node(last_created_node,self.active_node.clone());
 
@@ -156,6 +184,7 @@ impl<'a, T: Eq + Hash + Display> UkkonenTree<'a, T> {
                 }
 
                 // Otherwise we have to break the path
+                println!("    breaking the path!");
 
                 // create a new node with proper beginning and ending index
                 let new_node_start = *active_child.start_idx.borrow();
@@ -175,20 +204,25 @@ impl<'a, T: Eq + Hash + Display> UkkonenTree<'a, T> {
 
                 // Assign the new node as child of active_node
                 let break_path_node = Rc::new(break_path_node);
-                self.active_node.children.borrow_mut().insert(self.sequence[new_node_start], Rc::clone(&break_path_node));
+                children_ref.insert(self.sequence[new_node_start], Rc::clone(&break_path_node));
 
                 // check if there was already a new node created in this tree extension step.
                 // If it was, we have to link the new node to it
                 self.link_created_node(last_created_node, Rc::clone(&break_path_node));
                 last_created_node = Some(break_path_node);
             }
+            drop(self.active_node.children.borrow_mut());
+
+            println!("    outside of `if`");
 
             self.remainder -= 1;
 
             // If root is the active node at this point it means we inserted a new node or leaf from it
 
             // If root is the active edge, and the active length is nonzero:
-            if !Rc::ptr_eq(&self.root, &self.active_node) {
+            if Rc::ptr_eq(&self.root, &self.active_node) && self.active_length > 0{
+                println!("    lowering the remainder");
+
                 // Select the active edge on the root using the 2nd character of active sequence
                 self.active_edge = Some(self.sequence.len() - self.remainder);
 
@@ -213,9 +247,8 @@ impl<'a, T: Eq + Hash + Display> UkkonenTree<'a, T> {
 
     fn bfs_print(&self, node: Rc<Node<'a, T>>, depth: usize) {
         let idx = node.start_idx.borrow().clone();
-        println!("{}{}", " ".repeat(depth*4), self.sequence[idx]);
+        println!("{}{} (idx {})", " ".repeat(depth*4), self.sequence[idx], idx);
 
-        println!("{}{}", " ".repeat(depth*4), node.children.borrow().len());
         for child in node.children.borrow().values() {
             self.bfs_print(Rc::clone(child), depth + 1);
         }
